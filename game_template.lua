1178644001ba76503c4b16d95180168ac387d6be
EliottmacR <47741786+EliottmacR@users.noreply.github.com>
2019-07-06 04:54:45 +0200

    new start

--- game_template.lua
+++ game_template.lua
@@ -1,6 +1,5 @@
 require("framework/framework.lua")
 require("socket")
-http = require("socket.http")
 _palette = {0, 17, 14, 13, 20, 4}
 
 _controls = {
@@ -22,65 +21,19 @@ _objects = {}
 
 local x,y = 128,96
 function _init()
-  local referrer = castle.game.getReferrer()
-  local params = castle.game.getInitialParams()
-  if params then
-    _objects = params.objects or {}
-    _game_registery = params._game_registery or {}
-  end
-  if not _game_registery then 
-    local g_r_url = "https://raw.githubusercontent.com/EliottmacR/Collection/master/game_registery"
-    require("socket")
-    local https = require("ssl.https")
-    local body, code, headers, status = https.request(g_r_url)
-    -- if body then
-      -- log(body)
-    -- else
-      -- log("Could not load other games")
-    -- end
-    -- if status then
-      -- log(status)
-    -- end
-    
-    _game_registery = {}
-    
-    line = body
 
-    for token in string.gmatch(line, "[^\n]+") do
-       -- print(token) 
-       add(_game_registery, token)
-    end
-    -- for i, v in pairs(_game_registery) do
-      -- log(i)
-      -- log(v)
-    -- end
-    
-    
-    
-  end
+
 end
 
 function _update()
   x = x - btnv("left") + btnv("right")
   y = y - btnv("up") + btnv("down")
   
-  if btnp("A") then
+  if btnp("A") or btnp("cur_rb") then
     add(_objects, {spr = 0x03,  p = {x = btnv("cur_x"), y = btnv("cur_y")}})  
   end
   
   if btnp("B") then
-    -- local url = "https://raw.githubusercontent.com/EliottmacR/Collection/master/game_template.castle"
-    local ind = irnd(#_game_registery) + 1
-    local url = _game_registery[ind]
-    log("portal to game number " .. ind)
-    log("url =  " .. url)
-    
-    castle.game.load(
-      url, {
-        objects = _objects,
-        _game_registery = _game_registery
-      }
-    )
   end
   
 end
@@ -88,8 +41,20 @@ end
 function _draw()
   cls(1)
   
-  glyph(0x03, 32, 32, 16, 16, 2*t(), 2, 3)
+  -- glyph(0x03, 32, 32, 16, 16, 2*t(), 2, 3)
   
+  -- games 
+  for i, game in pairs(_game_registery) do
+    rectfill(0,(i)*24, str_px_width(game.name), (i+1)*24, flr(t()* 3) + 1)
+    print(game.name, 0, i*24, flr(t()* 3))  
+    glyph(game.player_spr, str_px_width(game.name), i*24, 16, 16, 2*t(), 2, 3) 
+    
+    if btnp("cur_lb") and point_in_rect(btnv("cur_x"),btnv("cur_y"), 0,i*24, str_px_width(game.name), (i+1)*24) then
+      go_to_game(i)
+    end
+  end
+  
+  -- objects
   for _, obj in pairs(_objects) do
     glyph(obj.spr, obj.p.x, obj.p.y, 16, 16, 2*t(), 2, 3)  
   end
@@ -101,4 +66,26 @@ function _draw()
 --  circ(btnv("cur_x"), btnv("cur_y"), btn("cur_rb") and 6 or btn("cur_lb") and 12 or 3, 4)
   outlined_glyph(0x00, btnv("cur_x"), btnv("cur_y"), 8 + 8 * btnv("cur_lb"), 8 + 8 * btnv("cur_rb"), 0, 4, 5, 0)
   
+  
+  
+  
+end
+
+
+function point_in_rect(xp, yp, x1, y1, x2, y2)
+  return xp > x1 and xp < x2 and yp > y1 and yp < y2
+end
+
+function go_to_game(index_game)
+  local url = _game_registery[index_game].url_main
+  
+  log("going to .. " .. _game_registery[index_game].name )    
+  log("url =  " .. url)
+  
+  castle.game.load(
+    url, {
+      objects = _objects,
+      _game_registery = _game_registery
+    }
+  )
 end
